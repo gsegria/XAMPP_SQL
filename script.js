a363c20173c33e832244d16c25e578504c311aff
gsegria <pinghsu1025@gmail.com>
2025-08-05 15:39:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,14 @@ function openPingHsu104() {
   window.open('https://www.104.com.tw/jobbank/custview/index.php?r=cust&j=4a4b432f3e5c3f284e323c1d1d1d1d5f2443a363189j99', '_blank', 'noopener,noreferrer');
 }
 
-function openPingHsuLinkedIn() {
+function openuLinkedIn() {
   // 這裡確保是在點擊事件中呼叫window.open，避免被阻擋
-  const newWindow = window.open('https://www.linkedin.com/in/ping-h-32b485104/', '_blank', 'noopener,noreferrer');
+  const newWindow = window.open('https://www.linkedin.com/feed/', '_blank', 'noopener,noreferrer');
   if (!newWindow) {
     alert('瀏覽器阻擋了彈出視窗，請允許彈窗後再試一次');
   }
+  
+function openuLinkedInPingHsu() {
+  window.open('https://www.linkedin.com/feed/', '_blank', 'noopener,noreferrer');
+}
 }
