340fbc672b5efbc230c09956e2eab2d883d0911c
gsegria <pinghsu1025@gmail.com>
2025-08-05 15:25:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+
 function showMessage() {
-    document.getElementById('message').textContent = 'You clicked the button!';
+    document.getElementById('message').innerText = "You clicked the button!";
 }
 
 function open104Home() {
-    window.open('https://www.104.com.tw', '_blank');
+    window.open('https://www.104.com.tw/', '_blank');
 }
 
 function openPingHsu104() {
-    window.open('https://www.104.com.tw/jobbank/custview/index.php?r=cust&j=4a4b432f3e5c3f284e323c1d1d1d1d5f2443a363189j99', '_blank');
+    window.open('https://pda.104.com.tw/profile/share/djwAyCBavZgVbIJ9Kz6ml70x9USFzk1v', '_blank');
 }
