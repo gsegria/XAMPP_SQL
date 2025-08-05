5bca64fe2a3e63b693cd200239446c434951a80b
gsegria <pinghsu1025@gmail.com>
2025-08-05 14:51:51 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 function showMessage() {
     document.getElementById('message').innerText = "You clicked the button!";
 }
+
+function open104Home() {
+    window.open('https://www.104.com.tw/', '_blank');
+}
+
+function openPingHsu104() {
+    window.open('https://pda.104.com.tw/profile/share/djwAyCBavZgVbIJ9Kz6ml70x9USFzk1v', '_blank');
+}
